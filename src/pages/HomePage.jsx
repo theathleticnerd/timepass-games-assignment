fc784661b85fd71ad93cf21278697207e3d29684
Job Fernandez <jobandjeffy@gmail.com>
2025-02-15 03:07:02 +0530

    feat: :lipstick: thank you screen

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useState, useMemo } from 'react';
 import DesktopBanner from 'src/assets/images/bg-sidebar-desktop.svg';
+import ThankYouScreen from 'src/components/ThankYouScreen';
 import InfoStep from 'src/components/InfoStep';
 import PlanStep from 'src/components/PlanStep';
 import AddonStep from 'src/components/AddonStep';
@@ -63,10 +64,15 @@ export default function HomePage() {
       setActiveStep(activeStep - 1);
     }
   };
+
+  const [isThankYouScreen, setIsThankYouScreen] = useState(false);
+  const showThankYouScreen = () => {
+    setIsThankYouScreen(true);
+  };
   return (
     <main className="max-w-7xl mx-auto h-svh relative">
       <div className="absolute flex bg-white w-4xl top-1/2 left-1/2 -translate-1/2 min-h-[32rem] rounded-xl shadow-lg px-3 py-3 justify-between space-x-24">
-        <div className=" bg-[url('src/assets/images/bg-sidebar-desktop.svg')] bg-no-repeat relative rounded-lg">
+        <div className=" bg-[url('src/assets/images/bg-sidebar-desktop.svg')] bg-no-repeat relative rounded-lg shrink-0">
           <div className="space-y-8 px-8 py-12 absolute">
             {steps.map((step) => (
               <div key={step.id} className="flex items-center space-x-3 text-white/90">
@@ -83,22 +89,28 @@ export default function HomePage() {
           </div>
           <img src={DesktopBanner} alt="Desktop Banner Image" className="invisible" />
         </div>
-        <div className="mr-20 flex flex-col justify-between grow pt-10 pb-6">
-          <h2 className="text-marine-blue text-3xl font-bold mb-2">{currentStep.title}</h2>
-          <p className="text-cool-gray mb-6 text-sm font-medium">{currentStep.subtitle}</p>
-          <div className="grow">{currentStep.component}</div>
-          <div className="flex justify-between items-center">
-            <button
-              className={`font-medium text-cool-gray ${activeStep === 1 ? 'invisible' : 'visible'} cursor-pointer`}
-              onClick={goToPreviousStep}>
-              Go Back
-            </button>
-            <button
-              className="px-6 bg-marine-blue py-3 text-white rounded-lg font-semibold text-sm cursor-pointer"
-              onClick={goToNextStep}>
-              {activeStep === steps.length ? 'Confirm' : 'Next Step'}
-            </button>
-          </div>
+        <div className="mr-20 grow flex">
+          {isThankYouScreen ? (
+            <ThankYouScreen />
+          ) : (
+            <div className="flex flex-col h-full justify-between pt-10 pb-6 grow">
+              <h2 className="text-marine-blue text-3xl font-bold mb-2">{currentStep.title}</h2>
+              <p className="text-cool-gray mb-6 text-sm font-medium">{currentStep.subtitle}</p>
+              <div className="grow">{currentStep.component}</div>
+              <div className="flex justify-between items-center">
+                <button
+                  className={`font-medium text-cool-gray ${activeStep === 1 ? 'invisible' : 'visible'} cursor-pointer`}
+                  onClick={goToPreviousStep}>
+                  Go Back
+                </button>
+                <button
+                  className="px-6 bg-marine-blue py-3 text-white rounded-lg font-semibold text-sm cursor-pointer"
+                  onClick={activeStep === steps.length ? showThankYouScreen : goToNextStep}>
+                  {activeStep === steps.length ? 'Confirm' : 'Next Step'}
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </main>
