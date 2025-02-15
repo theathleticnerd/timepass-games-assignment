199da5cf94e0fdf2e4414dee6e712be49cc58006
Job Fernandez <jobandjeffy@gmail.com>
2025-02-15 17:26:29 +0530

    feat: :sparkles: text field

--- src/components/InfoStep.jsx
+++ src/components/InfoStep.jsx
@@ -1,10 +1,54 @@
+import { useState } from 'react';
 import TextField from 'src/components/TextField';
 export default function InfoStep() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  const [isFormValid, setIsFormValid] = useState(false);
+  const saveInfo = () => {
+    setIsFormValid(true);
+    if (!name) {
+      console.log('Name is required.');
+      setIsFormValid(false);
+    }
+    if (!phoneNumber) {
+      console.log('Phone Number is required');
+      setIsFormValid(false);
+    }
+
+    if (isFormValid) {
+      console.log('Go To Next Step');
+    }
+  };
   return (
     <div className="space-y-8">
-      <TextField label="Name" placeholder="e.g. Stephen King" />
-      <TextField label="Email Address" placeholder="e.g. stephenking@lorem.com" />
-      <TextField label="Phone Number" placeholder="e.g. +1 234 567 890" />
+      <TextField
+        label="Name"
+        value={name}
+        placeholder="e.g. Stephen King"
+        onChange={(event) => setName(event.target.value)}
+        required
+      />
+      <TextField
+        type="email"
+        value={email}
+        label="Email Address"
+        placeholder="e.g. stephenking@lorem.com"
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <TextField
+        label="Phone Number"
+        value={phoneNumber}
+        placeholder="e.g. +1 234 567 890"
+        onChange={(event) => {
+          setPhoneNumber(event.target.value);
+        }}
+        required
+      />
+      <p>{name}</p>
+      <p>{email}</p>
+      <p>{phoneNumber}</p>
     </div>
   );
 }
--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,20 +1,32 @@
+import { useState } from 'react';
+
 export default function TextField(props) {
   const label = props.label || null;
+  const type = props.type || 'text';
+  const value = props.value || null;
+  const onChange = props.onChange;
   const placeholder = props.placeholder || null;
   const required = props.required || false;
-  const errorMessage = props.errorMessage || null;
+  // const errorMessage = props.errorMessage || null;
+
+  const [touched, setTouched] = useState(false);
 
   return (
     <div>
       <div className="flex mb-1.5 items-center justify-between">
         {label && <label className="text-sm text-marine-blue ">{label}</label>}
-        {required && (
+        {touched && required && !value && (
           <p className="text-sm font-semibold text-strawberry-red"> This field is required </p>
         )}
       </div>
       <input
-        className="border border-light-gray w-full h-12 px-2.5 rounded-lg font-medium"
+        type={type}
+        value={value}
+        onChange={onChange}
+        required={required}
         placeholder={placeholder}
+        onBlur={() => setTouched(true)}
+        className={`border border-light-gray w-full h-12 px-2.5 rounded-lg font-medium ${touched && required && !value && 'border-strawberry-red'}`}
       />
     </div>
   );
